472cd781ee6209d892b30ea458e8c670178f7e0f
sameeksh-komatreddy <mikkikr14@gmail.com>
2026-02-28 02:24:41 -0600

    Add my introduction lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Sameeksh" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone! I’m Sameeksh, a junior at Coppell High School. I enjoy learning new things related to physics and artificial intelligence. I’m currently working on making my own ML models and am a part of my school’s physics club. In my free time, I like to read, watch F1, or play video games. A fun fact about me is that I also enjoy playing table tennis.")
 
 if __name__ == "__main__":
     main()
